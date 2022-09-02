f640dbc2d5d4bb2ab817a02477c1159f724ef827
qusr08 <34079525+qusr08@users.noreply.github.com>
2022-09-02 03:58:23 -0400

    added featured project selection + minor changes

--- dev/js/custom-elements.js
+++ dev/js/custom-elements.js
@@ -9,20 +9,27 @@ window.customElements.define('fa-gallery-item', class extends HTMLElement {
         let desc = this.getAttribute('desc');
         let image = this.getAttribute('image');
         let link = this.getAttribute('link');
-        let tags = this.getAttribute('tags').trim().split(',');
+        let tags = this.getAttribute('tags');
         let info = this.getAttribute('info');
 
         let tagsHTML = "";
-        tags.forEach(tag => {
-            tagsHTML += `<p class="gallery-em">${tag.trim()}</p>`;
-        });
+        if (tags != undefined) {
+            tags.trim().split(',').forEach(tag => {
+                tagsHTML += `<p class="gallery-em">${tag.trim()}</p>`;
+            });
+        }
 
         let infoHTML = "";
         if (info != undefined) {
-            infoHTML = `<p class="gallery-item-info">${info}</p>`;
+            infoHTML += `<div class="gallery-item-info">`;
+            info.trim().split(',').forEach(item => {
+                infoHTML += `<p>${item.trim()}</p>`;
+            });
+            infoHTML += `</div>`;
         }
 
         this.style.backgroundImage = `url('${image}')`;
+        this.style.display = (infoHTML.indexOf("Featured") != -1 ? "unset" : "none");
 
         this.innerHTML = `
             <a class="gallery-item vert-list" style="background-image: url('${image}');" href="${link}">
@@ -37,3 +44,18 @@ window.customElements.define('fa-gallery-item', class extends HTMLElement {
         `;
     }
 });
+
+function toggleFeaturedGalleryItems() {
+    let galleryItems = document.getElementById('gallery').children;
+
+    for (let i = 0; i < galleryItems.length; i++) {
+        let infoAttr = galleryItems[i].getAttribute('info');
+
+        if (infoAttr != undefined && infoAttr.indexOf("Featured") != -1) {
+            continue;
+        }
+
+        let display = galleryItems[i].style.display;
+        galleryItems[i].style.display = (display == "unset" ? "none" : "unset");
+    }
+}
