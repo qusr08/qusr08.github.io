827488e8432487a4b7320b2fbd6709531a180edf
qusr08 <34079525+qusr08@users.noreply.github.com>
2022-09-08 02:22:19 -0400

    Added text gallery to dev

--- dev/js/custom-elements.js
+++ dev/js/custom-elements.js
@@ -45,17 +45,64 @@ window.customElements.define('fa-gallery-item', class extends HTMLElement {
     }
 });
 
-function toggleFeaturedGalleryItems() {
-    let galleryItems = document.getElementById('gallery').children;
+window.customElements.define('fa-text-gallery-item', class extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        let name = this.getAttribute('name');
+        let dates = this.getAttribute('dates');
+        let status = this.getAttribute('status');
+        let desc = this.getAttribute('desc');
+        let tags = this.getAttribute('tags');
+
+        let tagsHTML = "";
+        if (tags != undefined) {
+            tags.trim().split(',').forEach(tag => {
+                tagsHTML += `<p class="gallery-em">${tag.trim()}</p>`;
+            });
+        }
+
+        this.innerHTML = `
+            <a class="text-gallery-item vert-list">
+                <div class="vert-list">
+                    <h1 class="gallery-em">${name}</h1>
+                    <h2><em>${status}</em></h2>
+                    <h3>${dates}</h3>
+                    <p>${desc}</p>
+                </div>
+                <div class="hori-list" style="flex-wrap: wrap;">${tagsHTML}</div>
+            </a>
+        `;
+    }
+});
+
+function toggleFeaturedGalleryItems(button) {
+    let gallery = document.querySelector('.gallery');
+    let isFeaturedGallery = gallery.classList.contains('featured');
+    let galleryItems = gallery.children;
 
     for (let i = 0; i < galleryItems.length; i++) {
+        // Get the info tags of the gallery item
         let infoAttr = galleryItems[i].getAttribute('info');
 
+        // Make sure to only set the visibility of the non-featured items
         if (infoAttr != undefined && infoAttr.indexOf("Featured") != -1) {
             continue;
         }
 
-        let display = galleryItems[i].style.display;
-        galleryItems[i].style.display = (display == "unset" ? "none" : "unset");
+        // Set the display of the element
+        galleryItems[i].style.display = (isFeaturedGallery ? "unset" : "none");
+    }
+
+    // Change the inner text of the toggle button
+    button.innerHTML = `${isFeaturedGallery ? "View Featured Projects": "View All Projects"}`;
+
+    // Add or remove the featured class from the gallery object so it toggles back and forth each time the button is pressed
+    if (isFeaturedGallery) {
+        gallery.classList.remove("featured");
+    } else {
+        gallery.classList.add("featured");
     }
 }
