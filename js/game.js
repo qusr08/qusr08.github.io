6bb5b9bec4388e4aefc2ff5a937a62a85c86fb75
Frank Alfano <q24xboss@gmail.com>
2022-09-29 17:29:27 -0400

    Version 3.0.5
    Fixed collision categories/masks for matter objects
    Added comments to game.js
    Removed old code from game.js

--- js/game.js
+++ js/game.js
@@ -1,5 +1,8 @@
 // https://brm.io/matter-js/docs/
 
+// https://stackoverflow.com/questions/9692448/how-can-you-find-the-centroid-of-a-concave-irregular-polygon-in-javascript
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+
 // Setup the app
 "use strict";
 
@@ -13,13 +16,19 @@ const REF_SHP_ANG_VEL = 0.05;
 
 const WRAPPER = document.getElementById("wrapper");
 const SHP_SPWN_RATE = 500;
-const S_SHP_ROT_SPD = 0.005;
 const MAX_SHPS = 200;
-
 const PEG_DEN = 0;
 
-let wrapperWidth, wrapperHeight;
-let widthRatio, heightRatio, ratio;
+const MOUSE_CONST_STIFF = 0.1;
+const MOUSE_CONST_DAMP = 0;
+const STAT_CONST_STIFF = 0.025;
+const STAT_CONST_DAMP = 0.1;
+
+// https://www.temiz.dev/blog/matter-js-collisions-explained
+const CAT_GAME = 2;
+const CAT_STAT = 4;
+
+let widthRatio, heightRatio;
 let shapeSize, shapeVelocity, shapeAngleVelocity;
 let mousePosition = { x: 0, y: 0 };
 
@@ -27,8 +36,7 @@ let gameObjects = [];
 let HTMLObjects = [];
 let pegVoids = [];
 let pegObjects = [];
-let mouseConstraint;
-let mouseObject;
+let mouseObject = [];
 
 let engine;
 let render;
@@ -37,10 +45,7 @@ let runner;
 /************************************************* METHODS *******************************************************/
 
 window.onresize = function (event) {
-    // window.location.reload();
-
     // Shift all gravity objects
-    // updateGameObjects();
     gameObjects.forEach(element => { Matter.Composite.remove(engine.world, element); });
     gameObjects = [];
 
@@ -48,12 +53,12 @@ window.onresize = function (event) {
     updateVariables();
 
     // Resize window and renderer
-    render.bounds.max.x = wrapperWidth;
-    render.bounds.max.y = wrapperHeight;
-    render.options.width = wrapperWidth;
-    render.options.height = wrapperHeight;
-    render.canvas.width = wrapperWidth;
-    render.canvas.height = wrapperHeight;
+    render.bounds.max.x = WRAPPER.offsetWidth;
+    render.bounds.max.y = WRAPPER.offsetHeight;
+    render.options.width = WRAPPER.offsetWidth;
+    render.options.height = WRAPPER.offsetHeight;
+    render.canvas.width = WRAPPER.offsetWidth;
+    render.canvas.height = WRAPPER.offsetHeight;
 
     // Recreate static objects
     createObjectsFromHTML();
@@ -63,11 +68,13 @@ window.onresize = function (event) {
 window.onmousemove = function (event) {
     mousePosition = { x: event.clientX + window.scrollX, y: event.clientY + window.scrollY };
 
-    if (mouseConstraint == undefined) {
+    // mouseObject[1] is the mouse constraint (not the mouse constraint object)
+    // If the constraint has not been set, don't try to change its position
+    if (mouseObject[1] == undefined) {
         return;
     }
 
-    Matter.Body.setPosition(mouseConstraint, mousePosition);
+    Matter.Body.setPosition(mouseObject[1], mousePosition);
 };
 
 function initializeGame() {
@@ -79,8 +86,8 @@ function initializeGame() {
     // Create a renderer
     render = Matter.Render.create({
         options: {
-            width: wrapperWidth,
-            height: wrapperHeight,
+            width: WRAPPER.offsetWidth,
+            height: WRAPPER.offsetHeight,
             wireframes: false,
             background: getComputedStyle(document.documentElement).getPropertyValue('--color1'),
             hasBounds: true
@@ -96,31 +103,22 @@ function initializeGame() {
     Matter.Runner.run(runner, engine);
 
     // Create mouse interaction object
-    mouseConstraint = Matter.Bodies.circle(mousePosition.x, mousePosition.y, 1, {
+    mouseObject[0] = Matter.Bodies.circle(mousePosition.x, mousePosition.y, 25, {
         render: { visible: false },
         collisionFilter: {
-            'group': -1,
-            'category': 2,
-            'mask': 0,
+            category: CAT_GAME,
+            mask: CAT_GAME
         },
-        isStatic: true
-    });
-    mouseObject = Matter.Bodies.circle(mousePosition.x, mousePosition.y, 25, {
-        render: { visible: false },
         isStatic: false
     });
-    Matter.Composite.add(engine.world, [mouseConstraint, mouseObject, Matter.Constraint.create({
-        bodyA: mouseConstraint,
-        bodyB: mouseObject,
-        stiffness: 0.1,
-        length: 0,
-        render: { visible: false }
-    })]);
+    Matter.Composite.add(engine.world, mouseObject[0]);
+    mouseObject[1] = createConstraints([mousePosition], MOUSE_CONST_STIFF, MOUSE_CONST_DAMP, mouseObject[0]);
 
+    // Add update functionality for matter
     Matter.Events.on(engine, 'afterUpdate', function (event) {
         // If one of the gravity shapes has reached the bottom of the website, destroy it
         for (let i = gameObjects.length - 1; i >= 0; i--) {
-            if (gameObjects[i].position.y > wrapperHeight + shapeSize) {
+            if (gameObjects[i].position.y > WRAPPER.offsetHeight + shapeSize) {
                 Matter.Composite.remove(engine.world, gameObjects[i]);
                 gameObjects.splice(i, 1);
             }
@@ -134,6 +132,7 @@ function initializeGame() {
         }
     }, SHP_SPWN_RATE);
 
+    // Create background object
     createObjectsFromHTML();
     createPegObjects();
 }
@@ -150,54 +149,17 @@ function createObjectsFromHTML() {
         let x = element.offsetLeft + (width / 2);
         let y = element.offsetTop + (height / 2);
 
-        // Rectangle vertices
-        let vertices = [{ x: -(width / 2), y: -(height / 2) }, { x: (width / 2), y: -(height / 2) }, { x: (width / 2), y: (height / 2) }, { x: -(width / 2), y: (height / 2) }];
-
-        // // Check for alterations in the vertices from html
-        // if (element.hasAttribute("matter-slant")) {
-        //     let mInfo = element.getAttribute("matter-slant").trim().split(/\s+/);
-
-        //     for (let i = 0; i < mInfo.length; i += 3) {
-        //         // 0: vertex to move
-        //         // 1: x to move vertex
-        //         // 2: y to move vertex
-
-        //         let xChange = 0;
-        //         let yChange = 0;
-
-        //         if (mInfo[i + 1].includes("deg")) {
-        //             xChange = height * Math.tan(parseInt(mInfo[i + 1]) * DEG_TO_RAD);
-        //         } else {
-        //             xChange = parseInt(mInfo[i + 1]);
-        //         }
-
-        //         if (mInfo[i + 2].includes("deg")) {
-        //             yChange = width * Math.tan(parseInt(mInfo[i + 2]) * DEG_TO_RAD);
-        //         } else {
-        //             yChange = parseInt(mInfo[i + 2]);
-        //         }
-
-        //         vertices[parseInt(mInfo[i])].x += xChange;
-        //         vertices[parseInt(mInfo[i])].y -= yChange;
-        //     }
-        // }
-
-        // // Get the center of the new vertices so the center of the object can be offset
-        // let offsetPosition = getVerticesCenter(vertices);
-
-        // // Create a static object
-        // let o = Matter.Bodies.fromVertices(x + offsetPosition.x, y + offsetPosition.y, vertices, {
-        //     // render: { fillStyle: getComputedStyle(element).backgroundColor },
-        //     render: { fillStyle: '#00000000' },
-        //     isStatic: true
-        // });
-
         // Create a static object
-        let o = Matter.Bodies.fromVertices(x, y, vertices, {
-            render: { fillStyle: '#00000000' },
+        let o = Matter.Bodies.rectangle(x, y, width, height, {
+            render: { fillStyle: getComputedStyle(element).backgroundColor },
+            collisionFilter: {
+                category: CAT_STAT,
+                mask: CAT_GAME | CAT_STAT
+            },
             isStatic: true
         });
 
+        // Add the object to the world
         Matter.Composite.add(engine.world, o);
         HTMLObjects.push(o);
         pegVoids.push([{ x: x, y: y }, (Math.max(width, height) / 2) + (shapeSize * 2)]);
@@ -216,27 +178,39 @@ function createRandomBody(x, y, size, options) {
 }
 
 function createPegObjects() {
+    // Clear current peg objects
     pegObjects.forEach(element => { Matter.Composite.remove(engine.world, element); });
     pegObjects = [];
 
+    // Create variables for how to start the peg generation
     let perlinNoise = new SimplexNoise();
-
     let gap = shapeSize * 2.5;
     let size = shapeSize / 4;
     let row = 0;
 
-    for (let y = (gap / 4); y < wrapperHeight; y += gap, row++) {
-        for (let x = (gap / 4) + (row % 2 == 0 ? gap / 2 : 0); x < wrapperWidth; x += gap) {
+    // Pegs are set on a shifted grid
+    // Each row of pegs is slightly offset
+    for (let y = (gap / 4); y < WRAPPER.offsetHeight; y += gap, row++) {
+        for (let x = (gap / 4) + (row % 2 == 0 ? gap / 2 : 0); x < WRAPPER.offsetWidth; x += gap) {
+            // If the perlin noise is not above a certain threshold, do not spawn a peg in that position
+            // This creates random gaps around the background so it isn't covered with pegs
+            // It also gives more room for the shapes to fall and move around
             if (perlinNoise.noise(x, y, 0) <= PEG_DEN) {
                 continue;
             }
 
+            // If the peg is trying to spawn in a position too close to an html element, do not let it spawn
             if (IsPegPositionInVoid(x, y)) {
                 continue;
             }
 
+            // Create the peg object
             let o = Matter.Bodies.circle(x, y, size, {
                 render: { fillStyle: getComputedStyle(document.documentElement).getPropertyValue('--color5') },
+                collisionFilter: {
+                    category: CAT_STAT,
+                    mask: CAT_GAME
+                },
                 isStatic: true
             });
 
@@ -249,6 +223,7 @@ function createPegObjects() {
 function IsPegPositionInVoid(x, y) {
     let isInVoid = false;
 
+    // Check to see if the peg is in range of an html element
     pegVoids.forEach(element => {
         if (Math.hypot(x - element[0].x, y - element[0].y) <= element[1]) {
             isInVoid = true;
@@ -258,12 +233,55 @@ function IsPegPositionInVoid(x, y) {
     return isInVoid;
 }
 
-function createGameObject() {
-    let x = Math.random() * wrapperWidth;
-    let y = -shapeSize * 2;
-    let options = { render: { fillStyle: getComputedStyle(document.documentElement).getPropertyValue('--color3') } };
+function createConstraints(points, stiffness, damping, object) {
+    let constraintObject = undefined;
 
-    let o = createRandomBody(x, y, shapeSize, options)
+    // Create an object for each point to bind the object to
+    points.forEach(point => {
+        // Create the static constraint object
+        constraintObject = Matter.Bodies.circle(point.x, point.y, 1, {
+            render: { visible: false },
+            collisionFilter: {
+                category: 8,
+                mask: 8
+            },
+            isStatic: true
+        });
+
+        // Link the constraint object to the object passed into the function
+        let constraint = Matter.Constraint.create({
+            bodyA: constraintObject,
+            bodyB: object,
+            pointB: { x: point.x - object.position.x, y: point.y - object.position.y },
+            stiffness: stiffness,
+            damping: damping,
+            length: 0,
+            render: { visible: false }
+        });
+
+        // Add both objects to the world
+        Matter.Composite.add(engine.world, [constraintObject, constraint]);
+    });
+
+    // Return the last created constraint object (basically only used for the mouse)
+    return constraintObject;
+}
+
+function createGameObject() {
+    // Create variables for the game object
+    let x = Math.random() * WRAPPER.offsetWidth;
+    let y = -shapeSize * 2;
+    let options = {
+        render: { fillStyle: getComputedStyle(document.documentElement).getPropertyValue('--color3') },
+        collisionFilter: {
+            category: CAT_GAME,
+            mask: CAT_GAME | CAT_STAT
+        }
+    };
+
+    // Create the game object
+    let o = createRandomBody(x, y, shapeSize, options);
+    // Apply a random force and rotation to the object
     Matter.Body.applyForce(o, o.position, { x: getRandomNumber(-shapeVelocity, shapeVelocity), y: 0 });
     Matter.Body.setAngularVelocity(o, getRandomNumber(-shapeAngleVelocity, shapeAngleVelocity));
 
@@ -272,9 +290,6 @@ function createGameObject() {
 }
 
 function updateVariables() {
-    wrapperWidth = WRAPPER.offsetWidth;
-    wrapperHeight = WRAPPER.offsetHeight;
-
     widthRatio = window.innerWidth / REF_WINDOW_WIDTH;
     heightRatio = window.innerHeight / REF_WINDOW_HEIGHT;
 
@@ -283,65 +298,6 @@ function updateVariables() {
     shapeAngleVelocity = widthRatio * REF_SHP_ANG_VEL;
 }
 
-// function updateGameObjects() {
-//     let newWidthRatio = window.innerWidth / REF_WINDOW_WIDTH;
-
-//     gameObjects.forEach(element => {
-//         // Convert the previous dimensions of the coordinates to the new dimensions
-//         let refX = map(element.position.x, 0, wrapperWidth, 0, WRAPPER.offsetWidth);
-//         let refY = map(element.position.y, 0, wrapperHeight, 0, WRAPPER.offsetHeight);
-//         let refScale = 1 + newWidthRatio - widthRatio;
-
-//         Matter.Body.setPosition(element, { x: refX, y: refY });
-//         Matter.Body.scale(element, refScale, refScale);
-//     });
-// }
-
 function getRandomNumber(min, max) {
     return Math.random() * (max - min) + min;
 }
-
-// function getRandomArrayValue(array) {
-//     return array[Math.floor(Math.random() * array.length)];
-// }
-
-// function map(number, inMin, inMax, outMin, outMax) {
-//     // https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
-
-//     return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
-// }
-
-// function getVerticesCenter(vertices) {
-//     // https://stackoverflow.com/questions/9692448/how-can-you-find-the-centroid-of-a-concave-irregular-polygon-in-javascript
-
-//     let first = vertices[0],
-//         last = vertices[vertices.length - 1];
-//     let addedValue = false;
-//     let twicearea = 0,
-//         x = 0,
-//         y = 0,
-//         nPts = vertices.length,
-//         p1, p2, f;
-
-//     if (first.x != last.x || first.y != last.y) {
-//         vertices.push(first);
-//         addedValue = true;
-//     }
-
-//     for (let i = 0, j = nPts - 1; i < nPts; j = i++) {
-//         p1 = vertices[i];
-//         p2 = vertices[j];
-//         f = p1.x * p2.y - p2.x * p1.y;
-//         twicearea += f;
-//         x += (p1.x + p2.x) * f;
-//         y += (p1.y + p2.y) * f;
-//     }
-
-//     f = twicearea * 3;
-
-//     if (addedValue) {
-//         vertices.pop();
-//     }
-
-//     return { x: x / f, y: y / f };
-// }
