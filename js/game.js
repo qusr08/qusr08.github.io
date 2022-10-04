761ca5afc25030d6533a64b17f3b9540bfafe089
qusr08 <34079525+qusr08@users.noreply.github.com>
2022-10-04 02:28:50 -0400

    (released) 3.1.0
    Made main page html elements bounce and react to falling shapes
    Refactored a lot of the code for game.js
    Updated resume
    Updated logo and favicon
    Minor improvements to css

--- js/game.js
+++ js/game.js
@@ -18,7 +18,7 @@ const BASE_SHAPE_VELOCITY = 0.01;
 const BASE_SHAPE_VELOCITY_ANGULAR = 0.05;
 
 const WRAPPER = document.getElementById("wrapper");
-const SHAPE_SPAWN_RATE = 50;
+const SHAPE_SPAWN_RATE = 500;
 const SHAPE_MAX_COUNT = 200;
 const PEG_THRESHOLD = 0;
 
@@ -27,10 +27,6 @@ const MOUSE_CONSTRAINT_DAMPING = 0;
 const HTML_CONSTRAINT_STIFFNESS = 0.025;
 const HTML_CONSTRAINT_DAMPING = 0.1;
 const HTML_CONSTRAINT_DISTANCE_MODIFIER = 12;
-
-const ANIMATION_EASING_CONTROLLER = bezier(0.25, 0.1, 0.25, 1.0);
-const ANIMATION_RATE = 1;
-const ANIMATION_TIME = getComputedStyle(document.documentElement).getPropertyValue("--transition-time").replace('s', '') * 1000;
 
 // https://www.temiz.dev/blog/matter-js-collisions-explained
 const CATEGORY_GAME = 2;
@@ -50,29 +46,13 @@ let render;
 let runner;
 let isInitialized = false;
 
-// Ideas for Version 3.1.0
-// - make a list in the beginning of all matter-html elements and their background colors
-//   - this will not change at all while the page is running, it will be set each time the page is loaded
-// - Matter.Query.point() for updating scale and color of matter button objects
-//   - this is tricky because i want to follow the same easing equation as used in css, so i need to implement that
-//   - when an object is hovered, it is placed in a "fade to color" array, and updated each frame
-//     - there will be some global value stored in the master list to track its progress
-//     - once it has reached the color, it is removed from the list
-//   - when an object is exited, it is placed in a "fade to original" array, and updated each frame
-//     - once it has reached its original color, it is removed from the list
-//   - the scale will also fade with the color, mimicing the css transition
-// - fix peg voids by making them square? basically just like a big border around the object with more space at the top
-// - rename variables/functions to be more clear
-
 /************************************************* METHODS *******************************************************/
 
-window.onresize = function (event) { updateGame(); };
+window.onresize = function(event) { updateGame(); };
 
-window.onmousemove = function (event) { if (isInitialized) { mouseMatterObject.update({ x: event.clientX + window.scrollX, y: event.clientY + window.scrollY }); } };
+window.onmousemove = function(event) { if (isInitialized) { mouseMatterObject.update({ x: event.clientX + window.scrollX, y: event.clientY + window.scrollY }); } };
 
 function initializeGame() {
-    updateVariables();
-
     // Create an engine
     engine = Matter.Engine.create();
 
@@ -91,17 +71,17 @@ function initializeGame() {
     render.canvas.style.imageRendering = 'pixelated';
     Matter.Render.run(render);
 
-    updateVariables();
-
     // Create a runner
     runner = Matter.Runner.create();
     Matter.Runner.run(runner, engine);
+
+    updateVariables();
 
     // Create mouse interaction object
     mouseMatterObject = new MouseMatterObject();
 
     // Add update functionality for matter
-    Matter.Events.on(engine, 'afterUpdate', function (event) {
+    Matter.Events.on(engine, 'afterUpdate', function(event) {
         // If one of the gravity shapes has reached the bottom of the website, destroy it
         for (let i = gameObjects.length - 1; i >= 0; i--) {
             if (gameObjects[i].position.y > WRAPPER.offsetHeight + shapeSize) {
@@ -112,7 +92,7 @@ function initializeGame() {
     });
 
     // Create a new shape after a set interval
-    setInterval(function () {
+    setInterval(function() {
         if (document.hasFocus() && gameObjects.length <= SHAPE_MAX_COUNT) {
             createGameObject();
         }
@@ -298,7 +278,7 @@ function updateGame() {
     }
 }
 
-function updateHTMLMatterObjects (timestamp) {
+function updateHTMLMatterObjects(timestamp) {
     HTMLMatterObjects.forEach(matterObject => {
         if (matterObject.willAnimate) {
             matterObject.update();
@@ -308,36 +288,30 @@ function updateHTMLMatterObjects (timestamp) {
     window.requestAnimationFrame(updateHTMLMatterObjects);
 }
 
-// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
 function map(number, inMin, inMax, outMin, outMax) {
+    // https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
     return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }
 
 class HTMLMatterObject {
     constructor(HTMLElement) {
+        // let _this = this;
         this.HTMLElement = HTMLElement;
+        this.willAnimate = HTMLElement.classList.contains("button");
+        this.backgroundColor = getComputedStyle(this.HTMLElement).backgroundColor;
+        if (this.willAnimate) {
+            this.backgroundColor = getComputedStyle(document.documentElement).getPropertyValue("--panel-color");
+        }
+
         this.width = -1;
         this.height = -1;
         this.x = -1;
         this.y = -1;
 
-        this.willAnimate = HTMLElement.classList.contains("button");
-
         this.body = undefined;
         this.pegVoid = undefined;
         this.constraintBodies = [undefined, undefined];
         this.constraints = [undefined, undefined];
-
-        // this.animationInterval = undefined;
-        // this.animationValue = 0;
-        // this.animateForward = true;
-
-        // let _this = this;
-        // if (this.willAnimate) {
-        //     setInterval(function () {
-        //         _this.update();
-        //     }, 1);
-        // }
 
         this.update();
     }
@@ -361,15 +335,9 @@ class HTMLMatterObject {
                 Matter.Composite.remove(engine.world, this.body);
             }
 
-            let backgroundColor = RGBA_2_HEX(getComputedStyle(this.HTMLElement).backgroundColor);
-            console.log(getComputedStyle(this.HTMLElement).backgroundColor);
-            if (this.willAnimate) {
-                backgroundColor = backgroundColor.slice(0, -2);
-            }
-
             // Create a matter body
             this.body = Matter.Bodies.rectangle(x, y, width, height, {
-                render: { fillStyle: backgroundColor },
+                render: { fillStyle: this.backgroundColor },
                 collisionFilter: {
                     category: CATEGORY_HTML,
                     mask: CATEGORY_GAME | CATEGORY_HTML
@@ -377,9 +345,7 @@ class HTMLMatterObject {
                 label: 'HTMLMatterObject'
             });
 
-            if (!this.willAnimate) {
-                this.HTMLElement.style.backgroundColor = 'transparent';
-            }
+            this.HTMLElement.style.backgroundColor = 'transparent';
 
             // Add the body to the world
             Matter.Composite.add(engine.world, this.body);
@@ -401,33 +367,6 @@ class HTMLMatterObject {
             Matter.Composite.add(engine.world, this.constraints);
         }
     }
-
-    // startAnimation(animateForward) {
-    //     this.animateForward = animateForward;
-
-    //     // Use _this to access class variables inside the timer function
-    //     let _this = this;
-
-    //     // Start the animation loop
-    //     this.animationInterval = setInterval(function() {
-    //         // let scaleValue = map(_this.animationValue, 0, 1, 1, 1.2);
-    //         // Matter.Body.scale(_this.body, scaleValue, 1);
-
-    //         _this.animationValue += (_this.animateForward ? 1 : -1) * (ANIMATION_RATE / ANIMATION_TIME);
-
-    //         // If the animation has reached the animation value it was trying to get to
-    //         if ((_this.animateForward && _this.animationValue >= 1) || (!_this.animateForward && _this.animationValue <= 0)) {
-    //             _this.stopAnimation();
-    //             console.log("awd");
-    //         }
-    //     }, ANIMATION_RATE);
-    // }
-
-    // stopAnimation () {
-    //     // Clear the animation loop
-    //     clearInterval(this.animationInterval);
-    //     this.animationInterval = undefined;
-    // }
 }
 
 class MouseMatterObject {
