de0d8f9ba5ec0124530a45b0e6a458d55f61b6de
Frank Alfano <q24xboss@gmail.com>
2025-03-07 23:27:25 -0500

    Reworked portfolio style

--- js/index.js
+++ js/index.js
@@ -12,23 +12,15 @@ window.onload = () => {
 }
 
 function createProjectBox(projectName, isReversed) {
-    let projectNameLowerCase = projectName.toLowerCase();
-
     // Create project box container element
-    let projectBoxContainer = document.createElement("div");
-    projectBoxContainer.classList.add("proj-box-container");
-    if (isReversed) projectBoxContainer.classList.add("reversed");
+    let projectContainer = document.createElement("div");
+    projectContainer.classList.add("proj-container");
+    if (isReversed) projectContainer.classList.add("reversed");
 
-    // Create project box element
-    let projectBox = document.createElement("a");
-    projectBox.href = `html/${projectNameLowerCase}.html`;
-    projectBox.classList.add("proj-box");
-
-    // Create project thumbnail element
-    let projectThumbnail = document.createElement("div");
-    projectThumbnail.classList.add("proj-thumbnail");
-    projectThumbnail.style.backgroundImage = `url('png/${projectNameLowerCase}/${projectNameLowerCase}-thumbnail.png')`;
-    projectBox.appendChild(projectThumbnail);
+    // Create project button element
+    let projectButton = document.createElement("div");
+    projectButton.classList.add("proj-button");
+    projectContainer.appendChild(projectButton);
 
     // Create project info element
     let projectInfo = document.createElement("div");
@@ -40,17 +32,26 @@ function createProjectBox(projectName, isReversed) {
     projectTitle.innerHTML = projectName;
     projectInfo.appendChild(projectTitle);
 
-    // Create project dates element
-    let projectDates = document.createElement("p");
-    projectDates.classList.add("proj-dates");
-    projectDates.innerHTML = PROJECT_DATA[projectName].dates;
-    projectInfo.appendChild(projectDates);
-
     // Create project description element
     let projectDescription = document.createElement("p");
     projectDescription.classList.add("proj-desc");
     projectDescription.innerHTML = PROJECT_DATA[projectName].desc;
     projectInfo.appendChild(projectDescription);
+
+    // Create project awards element
+    let projectAwards = document.createElement("div");
+    projectAwards.classList.add("proj-tags");
+    PROJECT_DATA[projectName].awards.forEach(award => {
+        // Create tag element
+        let projectAward = document.createElement("p");
+        projectAward.classList.add("proj-award");
+        if (award.includes("GMTK Game Jam 2022")) projectAward.classList.add("gmtk-2022");
+        if (award.includes("GMTK Game Jam 2021")) projectAward.classList.add("gmtk-2021");
+        if (award.includes("RIT EDGE")) projectAward.classList.add("rit-edge");
+        projectAward.innerHTML = award;
+        projectAwards.appendChild(projectAward);
+    });
+    projectInfo.appendChild(projectAwards);
 
     // Create project tags element
     let projectTags = document.createElement("div");
@@ -59,51 +60,12 @@ function createProjectBox(projectName, isReversed) {
         // Create tag element
         let projectTag = document.createElement("p");
         projectTag.classList.add("proj-tag");
-        if (tag.includes("GMTK Game Jam 2022")) projectTag.classList.add("gmtk-2022");
-        if (tag.includes("GMTK Game Jam 2021")) projectTag.classList.add("gmtk-2021");
-        if (tag.includes("RIT EDGE")) projectTag.classList.add("rit-edge");
         projectTag.innerHTML = tag;
         projectTags.appendChild(projectTag);
     });
     projectInfo.appendChild(projectTags);
-
-    // Create project links element
-    let projectLinks = document.createElement("div");
-    projectLinks.classList.add("proj-links");
-    PROJECT_DATA[projectName].links.forEach(link => {
-        // Create link element
-        let projectLink = document.createElement("a");
-        projectLink.classList.add("proj-link");
-        projectLink.href = link.url;
-
-        // Create link icon element
-        let projectLinkIcon = document.createElement("img");
-        let iconType = "";
-        if (link.name.includes("Play")) iconType = "play";
-        if (link.name.includes("Github")) iconType = "github";
-        if (link.name.includes("Itch.io") || link.name.includes("GMTK")) iconType = "itchio";
-        if (link.name.includes("GMTK 2022")) projectLink.classList.add("gmtk-2022");
-        if (link.name.includes("GMTK 2021")) projectLink.classList.add("gmtk-2021");
-        if (link.name.includes(".com")) iconType = "link";
-        if (link.name.includes("RIT EDGE")) {
-            iconType = "youtube";
-            projectLink.classList.add("rit-edge");
-        }
-        projectLinkIcon.src = `png/logos/${iconType}-logo.png`;
-        projectLink.appendChild(projectLinkIcon);
-
-        // Create link name element
-        let projectLinkName = document.createElement("p");
-        projectLinkName.innerHTML = link.name;
-        projectLink.appendChild(projectLinkName);
-
-        // Append the final link element to the links list
-        projectLinks.appendChild(projectLink);
-    });
-    projectInfo.appendChild(projectLinks);
-
+    
     // Append the project box to the html document
-    projectBox.appendChild(projectInfo);
-    projectBoxContainer.appendChild(projectBox);
-    document.querySelector("div.proj-container").appendChild(projectBoxContainer);
+    projectContainer.appendChild(projectInfo);
+    document.querySelector("div.projects").appendChild(projectContainer);
 }
