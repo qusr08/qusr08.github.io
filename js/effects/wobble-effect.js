9f1bf6be847da9816358e6f6dc015b6fb399096d
Frank Alfano <q24xboss@gmail.com>
2026-06-08 15:58:52 -0400

    Reorganized project page information

--- js/effects/wobble-effect.js
+++ js/effects/wobble-effect.js
@@ -4,18 +4,20 @@ export default class MorphSquareElement {
         this.wobbleAmount = parseFloat(element.getAttribute("wobble-amount")) || 5;
         this.minWobbleSpeed = parseFloat(element.getAttribute("min-wobble-speed")) || 0.1;
         this.maxWobbleSpeed = parseFloat(element.getAttribute("max-wobble-speed")) || 0.2;
+        this.onlyVertical = (element.getAttribute("only-vertical") || "false") == "true";
+        this.onlyHorizontal = (element.getAttribute("only-horizontal") || "false") == "true";
 
-        this.offsets = [
-            { fromX: 0, fromY: 0, atX: 0, atY: 0, toX: 0, toY: 0, t: 0, spd: 0 },
-            { fromX: 0, fromY: 0, atX: 0, atY: 0, toX: 0, toY: 0, t: 0, spd: 0 },
-            { fromX: 0, fromY: 0, atX: 0, atY: 0, toX: 0, toY: 0, t: 0, spd: 0 },
-            { fromX: 0, fromY: 0, atX: 0, atY: 0, toX: 0, toY: 0, t: 0, spd: 0 }
-        ];
         this.bounds = [
             { minX: 0, minY: 0, maxX: this.wobbleAmount, maxY: this.wobbleAmount }, // Top Left
             { minX: 100 - this.wobbleAmount, minY: 0, maxX: 100, maxY: this.wobbleAmount }, // Top Right
             { minX: 100 - this.wobbleAmount, minY: 100 - this.wobbleAmount, maxX: 100, maxY: 100 }, // Bottom Right
             { minX: 0, minY: 100 - this.wobbleAmount, maxX: this.wobbleAmount, maxY: 100 } // Bottom Left
+        ];
+        this.offsets = [
+            { fromX: 0, fromY: 0, atX: 0, atY: 0, toX: 0, toY: 0, t: 0, spd: 0 },
+            { fromX: 100, fromY: 0, atX: 100, atY: 0, toX: 100, toY: 0, t: 0, spd: 0 },
+            { fromX: 100, fromY: 100, atX: 100, atY: 100, toX: 100, toY: 100, t: 0, spd: 0 },
+            { fromX: 0, fromY: 100, atX: 0, atY: 100, toX: 0, toY: 100, t: 0, spd: 0 }
         ];
 
         this.setRandomOffset(0, true);
@@ -47,12 +49,18 @@ export default class MorphSquareElement {
         let offset = this.offsets[i];
         let bound = this.bounds[i];
 
-        offset.fromX = setFrom ? randFloat(bound.minX, bound.maxX) : offset.toX;
-        offset.fromY = setFrom ? randFloat(bound.minY, bound.maxY) : offset.toY;
-        offset.atX = offset.fromX;
-        offset.atY = offset.fromY;
-        offset.toX = randFloat(bound.minX, bound.maxX);
-        offset.toY = randFloat(bound.minY, bound.maxY);
+        if (!this.onlyVertical) {
+            offset.fromX = setFrom ? randFloat(bound.minX, bound.maxX) : offset.toX;
+            offset.atX = offset.fromX;
+            offset.toX = randFloat(bound.minX, bound.maxX);
+        }
+
+        if (!this.onlyHorizontal) {
+            offset.fromY = setFrom ? randFloat(bound.minY, bound.maxY) : offset.toY;
+            offset.atY = offset.fromY;
+            offset.toY = randFloat(bound.minY, bound.maxY);
+        }
+
         offset.t = 0;
         offset.spd = randFloat(this.minWobbleSpeed, this.maxWobbleSpeed);
 
